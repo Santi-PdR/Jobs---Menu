b5c4988f4cddfb042afa8a6500e9f5cd684aec67
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-08-31 20:46:54 -0300

    polish: separate chat controls from document header

--- src/main/java/com/santipdr/jobsmenu/client/screen/PantallaChatJobs.java
+++ src/main/java/com/santipdr/jobsmenu/client/screen/PantallaChatJobs.java
@@ -24,10 +24,12 @@ public final class PantallaChatJobs extends ChatOptionsScreen {
         if (this.list != null) {
             this.list.setRenderBackground(false);
             this.list.setRenderTopAndBottom(false);
+            this.list.updateSize(this.width, this.height, 50, this.height - 42);
         }
         for (var child : this.children()) {
             if (child instanceof Button b && b.getMessage().equals(CommonComponents.GUI_DONE)) {
                 b.visible = false;
+                b.active = false;
                 break;
             }
         }
